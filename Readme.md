7abed766a6e112a23a31a580e6ed4db57cfd6c9f
sheolseol <siolsiol@hotmail.com>
2019-08-15 00:50:00 -0400

    1. Connect db 2. Migration product

--- Readme.md
+++ Readme.md
@@ -1,3 +1,17 @@
 #WeaverCore
 
 ## Web Backend
+
+## Run this commands by command line in the same folde is the web project:
+
+
+- dotnet ef database update
+- dotnet ef migrations add InitialDb
+- dotnet ef database update
+
+## Migration
+
+- dotnet ef migrations add ModifyProducts
+- dotnet ef database update
+
+
---
+++ Weaver/Weaver.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Weaver.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Weaver.Web.Data.Entities;
+
+    public class DataContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }   
+    }
+}
