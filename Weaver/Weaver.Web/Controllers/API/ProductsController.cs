b258187b7bf1673bfd97aa0b94b811512754156e
sheolseol <siolsiol@hotmail.com>
2019-09-09 22:13:06 -0400

    Create product

--- Weaver/Weaver.Web/Controllers/API/ProductsController.cs
+++ Weaver/Weaver.Web/Controllers/API/ProductsController.cs
@@ -1,11 +1,10 @@
 ﻿namespace Weaver.Web.Controllers.API
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
+    using System.IO;
     using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using Weaver.Web.Data.Entities;
     using Weaver.Web.Data.Repositories;
 
     [Route("api/[controller]")]
@@ -23,6 +22,29 @@
         public IActionResult GetProducts()
         {
             return Ok(this.productRepository.GetAll());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostProduct([FromBody] Common.Models.Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest(ModelState);
+            }
+                                 
+            var entityProduct = new Product
+            {
+                IsAvailabe = product.IsAvailabe,
+                LastPurchase = product.LastPurchase,
+                LastSale = product.LastSale,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+                ImageUrl = product.ImageUrl
+            };
+
+            var newProduct = await this.productRepository.CreateAsync(entityProduct);
+            return Ok(newProduct);
         }
     }
 }
